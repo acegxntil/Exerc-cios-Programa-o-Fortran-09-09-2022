ea5ebb8946654e965ee0dc94a33f9b877e50e0b1
Daniel <64984840+acegxntil@users.noreply.github.com>
2022-09-09 19:33:56 -0300

    Add files via upload

---
+++ main.f95
@@ -0,0 +1,14 @@
+!programa capaz de escrever na tela
+
+program sequencianumerica
+implicit none 
+
+integer i, sequencia
+
+!números naturais
+do i = 1,10
+sequencia = i
+print*, sequencia
+end do
+
+end program
